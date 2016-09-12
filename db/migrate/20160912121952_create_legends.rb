65b8c113a6ab8876dffa18465388dca2e5185ec5
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-09-12 14:30:35 +0200

    add migration for legends

---
+++ db/migrate/20160912121952_create_legends.rb
@@ -0,0 +1,26 @@
+Sequel.migration do
+  up do
+    create_table :legends do
+      Uuid :id, primary_key: true, default: 'uuid_generate_v4()'.lit
+      foreign_key :layer_id, :layers, type: 'uuid', null: false, on_delete: :cascade
+
+      String :title, null: false
+      String :prehtml, null: false
+      String :posthtml, null: false
+      String :migration_html
+      String :properties, null: false, type: 'json'
+    end
+
+    alter_table :legends do
+      add_index :layer_id
+    end
+  end
+
+  down do
+    alter_table :legends do
+      drop_index :layer_id
+    end
+
+    drop_table :legends
+  end
+end
