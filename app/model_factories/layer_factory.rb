c145f925d8cff529b46d19a8af22fa7a54824f71
Guido Fioravantti <guido.fioravantti@gmail.com>
2016-09-15 10:20:51 +0200

    Revert "9478 backend styles"

--- app/model_factories/layer_factory.rb
+++ app/model_factories/layer_factory.rb
@@ -4,7 +4,6 @@ require_relative '../../lib/carto/styles/style'
 require_relative '../../lib/carto/styles/point'
 require_relative '../../lib/carto/styles/line'
 require_relative '../../lib/carto/styles/polygon'
-require_relative '../../lib/carto/form'
 
 module ModelFactories
   class LayerFactory
@@ -32,11 +31,6 @@ module ModelFactories
       data_layer.infowindow['fields'] = []
       data_layer.tooltip ||= {}
       data_layer.tooltip['fields'] = []
-
-      if user.force_builder?
-        data_layer.options['style_properties'] = style_properties(user, geometry_type)
-      end
-
       data_layer
     end
 
@@ -53,13 +47,6 @@ module ModelFactories
           'name' => "#{base_layer.options['name']} Labels"
         )
       )
-    end
-
-    def self.style_properties(geometry_type)
-      {
-        type: 'simple',
-        properties: Carto::Form.new(geometry_type).to_hash
-      }
     end
 
     def self.tile_style(user, geometry_type)
